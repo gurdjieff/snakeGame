282c44d2ef0196c9448f455ed1e6db8c11d72526
DaiyuZhang <gurdjieff100@gmail.com>
2014-11-12 05:50:52 +0000

    Initial Commit

---
+++ Snake/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Snake
+//
+//  Created by daiyuzhang on 14-11-12.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
