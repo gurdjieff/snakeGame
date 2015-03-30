7e5c5959d59d2c23cba51358da1fb1557303598a
DaiyuZhang <gurdjieff100@gmail.com>
2015-03-30 20:31:12 +0100

    finished

--- Snake/AppDelegate.m
+++ Snake/AppDelegate.m
@@ -269,9 +269,8 @@
     if(lpNetstatus == kReachableViaWiFi) {
         [self sentCrashDataToParse];
         [self sentScoresDataToParse];
-        [VersionUpdateAssistant updateAppVersionInfomation];
     }
-
+    [VersionUpdateAssistant updateAppVersionInfomation];
 
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
